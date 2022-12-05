f7dc21a2a916ff664482ef20a20ddc4a3ae09e33
Benjamin Schubert <brm.schubert@gmail.com>
2022-12-05 00:36:07 -0500

    Day 5 part 2

--- day05/part2.go
+++ day05/part2.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 
 	advent "github.com/BenJuan26/advent2022"
 )
@@ -12,5 +14,59 @@ func Part2() {
 		panic(err)
 	}
 
-	fmt.Println(lines[0])
+	stacks := []stack{}
+	for i := 0; i < 9; i++ {
+		stacks = append(stacks, "")
+	}
+
+	re := regexp.MustCompile(`move (\d+) from (\d+) to (\d+)`)
+	reversed := false
+	for _, line := range lines {
+		if len(line) > 0 && line[0] == '[' {
+			for n := 0; n < 9; n++ {
+				i := 1 + (n * 4)
+				if i < len(line) && line[i] != ' ' {
+					stacks[n] = stacks[n].Push(rune(line[i]))
+				}
+			}
+		} else if len(line) > 0 && line[0:4] == "move" {
+			match := re.FindStringSubmatch(line)
+			amount, err := strconv.Atoi(match[1])
+			if err != nil {
+				panic(err)
+			}
+
+			from, err := strconv.Atoi(match[2])
+			if err != nil {
+				panic(err)
+			}
+
+			to, err := strconv.Atoi(match[3])
+			if err != nil {
+				panic(err)
+			}
+
+			from -= 1
+			to -= 1
+
+			tempStack := stack("")
+			for i := 0; i < amount; i++ {
+				var value rune
+				stacks[from], value = stacks[from].Pop()
+				tempStack = tempStack.Push(value)
+			}
+			tempStack = tempStack.Reverse()
+			stacks[to] = stacks[to] + tempStack
+		} else if !reversed {
+			for i := range stacks {
+				stacks[i] = stacks[i].Reverse()
+			}
+			reversed = true
+		}
+	}
+
+	for _, stack := range stacks {
+		fmt.Print(string(stack[len(stack)-1]))
+	}
+	fmt.Printf("\n")
 }
