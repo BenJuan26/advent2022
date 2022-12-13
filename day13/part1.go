21ee439978a9accc65f16109fcc5a3ff95d3e5a2
Benjamin Schubert <brm.schubert@gmail.com>
2022-12-13 00:56:06 -0500

    Day 13 silliness

--- day13/part1.go
+++ day13/part1.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"strconv"
@@ -48,25 +47,82 @@ func ParseList(s string) ([]interface{}, int) {
 	panic(errors.New("should have found a closing bracket"))
 }
 
+var ErrWrongOrder = errors.New("wrong order")
+
+func Compare(left, right interface{}) (bool, error) {
+	if l, ok := left.(int); ok {
+		if r, ok := right.(int); ok {
+			if l < r {
+				return true, nil
+			}
+		} else {
+			// left is int, right is list
+			rList, ok := right.([]interface{})
+			if !ok {
+				panic(fmt.Errorf("right value %+v was not an int or a list", right))
+			}
+			lList := []int{l}
+			return Compare(lList, rList)
+		}
+	} else {
+		lList, ok := left.([]interface{})
+		if !ok {
+			panic(fmt.Errorf("left value %+v was not an int or a list", left))
+		}
+		if r, ok := right.(int); ok {
+			if l < r {
+				return Compare(lList, []int{r})
+			}
+		} else {
+			// both lists
+			rList, ok := right.([]interface{})
+			if !ok {
+				panic(fmt.Errorf("right value %+v was not an int or a list", right))
+			}
+			i := 0
+			for i < len(lList) && i < len(rList) {
+				result, err := Compare(lList[i], rList[i])
+				if err != nil || result == true {
+					return result, err
+				}
+				i += 1
+				if i == len(lList) && i < len(rList) {
+					// left ran out first
+					return true, nil
+				}
+				if i < len(lList) && i == len(rList) {
+					// right ran out first
+					return false, ErrWrongOrder
+				}
+			}
+		}
+	}
+
+	return false, errors.New("unreachable?")
+}
+
 func Part1() {
 	lines, err := advent.ReadInput()
 	if err != nil {
 		panic(err)
 	}
 
-	// for i := 0; i < len(lines); i += 3 {
-	// 	// remove the outside brackets since an outer list is always guaranteed
-	// 	left := ParseList(lines[i])
-	// 	right := lines[1 : len(lines[i+1])-2]
+	total := 0
+	for i := 0; i < len(lines); i += 3 {
+		// remove the outside brackets since an outer list is always guaranteed
+		left, _ := ParseList(lines[i])
+		right, _ := ParseList(lines[i+1])
 
-	// }
-
-	parsed, _ := ParseList(lines[21])
-	b, err := json.Marshal(parsed)
-	if err != nil {
-		panic(err)
+		result, err := Compare(left, right)
+		if err != nil && err.Error() != ErrWrongOrder.Error() {
+			panic(err)
+		}
+		if err == nil && result == true {
+			total += (i / 3) + 1
+		}
 	}
-	fmt.Println(string(b))
+
+	fmt.Println(total)
 }
 
 func main() {
